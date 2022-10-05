984a5b077aac1bc1fd1fd75978783abfaf698eab
saitgungor <osmansaitgungor@gmail.com>
2022-10-05 21:03:04 +0300

    readme file and presentation gif added

--- helper/helper.js
+++ helper/helper.js
@@ -1,5 +1,4 @@
 export const sendPostRequest = (URL, configurationFile) => {
-  console.log(`URL: ${URL}`, configurationFile);
   fetch(
     `${URL || 'https://hook.eu1.make.com/frwu77c5mrw4acstxy0caxgp9b2a1grf'}`,
     {
